40258a1febb27ef23befab51c0fc5568e590a58a
tyler931101 <tylersnow931101@gmail.com>
2026-02-16 09:36:00 +0000

    fly away

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
-use marcroquad::prelude::*;
+use macroquad::prelude::{next_frame, *};
 
 #[macroquad::main("VoIP")]
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
+
+    loop {
+        clear_background(DARKPURPLE);
+
+        if is_key_down(KeyCode::Left) {
+            x -= 2.0;
+        }
+        if is_key_down(KeyCode::Right) {
+            x += 2.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 2.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 2.0;
+        }
+
+        draw_rectangle(x, y, 16.0, 16.0, BLUE);
+
+        next_frame().await;
+    }
 }
